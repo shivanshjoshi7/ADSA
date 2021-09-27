3fb45703286fbcc9f471964e506539899ab76d37
shivanshjoshi7 <90916063+shivanshjoshi7@users.noreply.github.com>
2021-09-27 12:26:49 +0530

    Add files via upload

---
+++ FindingSumCC.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;
+
+		int arr[n];
+		for(int i=0; i<n; i++)
+			cin>>arr[i];
+		
+		unordered_map<int,int> map;
+		map[0]=-1;
+
+		int count=0;
+		int pindex=-1;
+		int psum=0;
+		for(int i=0; i<n; i++)
+		{
+			psum+=arr[i];
+			map[psum]=i;
+			if(map.count(psum-k) && map[psum-k]>=pindex)
+			{
+				count++;
+				pindex=i;
+			}
+			
+		}
+		cout<<count<<endl;
+
+	}
+}
---
+++ PowerSetCC.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	long long int ans;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		ans=1;
+		for(int i=1; i<n; i++)
+		{
+		    ans=ans<<1;
+		    ans=ans%1000000007;
+		}
+		ans=ans>>1;
+		ans=(ans*n)%1000000007;
+		ans=(ans*(n+1))%1000000007;
+		
+		cout<<ans<<endl;
+	}
+	return 0;
+}
